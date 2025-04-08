3b0669919c8942a40d2bdec481e6522b9d17337b
johnluzada97 <johnluzada97@gmail.com>
2025-04-08 16:36:40 -0400

    put a dataframe in the new-file

--- new-file.py
+++ new-file.py
@@ -5,4 +5,14 @@ print("Hello World")
 for i in range(5):
     m = i**i
 
-    print(m + "hello")
+    print(m)
+
+
+import pandas as pd
+
+dataframe_a = { 'A' : [1, 2, 3, 4, 5, 6, 7] , 'B' : [1, 2, 3, 4, 5, 6, 7], 'C' : [1, 2, 3, 4, 5, 6, 7], 'D' : [1, 2, 3, 4, 5, 6, 7] }
+
+
+df = pd.DataFrame(dataframe_a)
+
+print(df)
